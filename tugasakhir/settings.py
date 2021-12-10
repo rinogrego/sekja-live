4d7ba148c13faebad3bf0f128056028c261fab47
rinogrego <68318284+rinogrego@users.noreply.github.com>
2021-12-10 10:40:30 +0700

    Update settings.py

--- tugasakhir/settings.py
+++ tugasakhir/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = ['127.0.0.1', 'sekja.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'sekja',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sekja',
 ]
 
 MIDDLEWARE = [
